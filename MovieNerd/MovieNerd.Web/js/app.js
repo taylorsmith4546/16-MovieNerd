0f2db7fd4752ea5cd4ab20eccdd1594d394cd711
Taylor Smith <taylorsmith4546@gmail.com>
2016-02-04 08:00:37 -0800

    first commit

---
+++ MovieNerd/MovieNerd.Web/js/app.js
@@ -0,0 +1,25 @@
+﻿angular.module('app', []);
+
+angular.module('app').controller('indexController', function ($scope, $http) {
+
+    $scope.message = 'Hello World';
+
+    $scope.searchTerm = '';
+
+    console.log('issuing http get');
+
+    $scope.searchForMovie = function () {
+        $http.get('http://www.omdbapi.com/?s=' + $scope.searchTerm)
+   .success(function (data) {
+       $scope.movies = data.Search;
+       console.log(data);
+   })
+   .error(function (error) {
+       alert(error);
+   });
+
+    };
+
+    });
+
+
